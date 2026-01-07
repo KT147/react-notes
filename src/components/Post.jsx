4a2ba75a6ae2592692495be1f9d55fa1146673c7
KT147 <kevin.tammm@gmail.com>
2026-01-07 14:17:44 +0200

    SinglePost

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,15 @@
-import classes from "./Post.module.css"
+import { Link } from "react-router-dom";
+import classes from "./Post.module.css";
 
 function Post(props) {
   return (
     <div className={classes.post}>
+      <Link to={props.id}>
         <p className={classes.author}>{props.author}</p>
         <p className={classes.text}>{props.body}</p>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
